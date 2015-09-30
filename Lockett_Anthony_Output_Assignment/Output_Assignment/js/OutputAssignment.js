3345dcd8ebabd9f1d200154cb6fa27bce06c40fa
ALockett89 <aklockett@fullsail.edu>
2015-09-30 11:53:13 -0500

    1st draft
    just starting

---
+++ Lockett_Anthony_Output_Assignment/Output_Assignment/js/OutputAssignment.js
@@ -0,0 +1,12 @@
+/*
+ Anthony Lockett
+ SDI Week 1
+ Output Assignment
+ 9/30/15
+ */
+
+alert("Here is my Week 1 submission! \n Output Assignment");// Just a friendly alert that I added.
+
+var myage = 26// This is my first number variable.
+var yearofbirth = 1989// This is another number variable.
+var favteam = "New Orleans Saints"// This is my string variable.
