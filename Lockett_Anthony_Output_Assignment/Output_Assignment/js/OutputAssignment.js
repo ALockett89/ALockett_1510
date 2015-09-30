a06e12a36eced71da9672b857884a5c34f764bc2
ALockett89 <aklockett@fullsail.edu>
2015-09-30 11:56:30 -0500

    2nd draft
    added more variables.

--- Lockett_Anthony_Output_Assignment/Output_Assignment/js/OutputAssignment.js
+++ Lockett_Anthony_Output_Assignment/Output_Assignment/js/OutputAssignment.js
@@ -10,3 +10,9 @@ alert("Here is my Week 1 submission! \n Output Assignment");// Just a friendly a
 var myage = 26// This is my first number variable.
 var yearofbirth = 1989// This is another number variable.
 var favteam = "New Orleans Saints"// This is my string variable.
+var losesouth = false// This is my first boolean variable.
+var superbowl = true// This is my second boolean variable.
+var record = "0 - 3"// This is my string variable.
+var driver = "Apple iPhone 6"// This is my string variable.
+var platform = "Xbox ONE"// This is my string variable.
+
