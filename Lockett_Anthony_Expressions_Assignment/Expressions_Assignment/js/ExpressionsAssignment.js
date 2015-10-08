4d02688e46c0210366359bb5f652f6579fc96d1b
ALockett89 <aklockett@fullsail.edu>
2015-10-08 17:30:35 -0500

    8th draft
    1st round of comments done

--- Lockett_Anthony_Expressions_Assignment/Expressions_Assignment/js/ExpressionsAssignment.js
+++ Lockett_Anthony_Expressions_Assignment/Expressions_Assignment/js/ExpressionsAssignment.js
@@ -5,18 +5,18 @@
  10/5/15
  */
 
-alert("          Here is my Week 2 submission! \n                Expressions Assignment");
+alert("          Here is my Week 2 submission! \n                Expressions Assignment");// Just a friendly alert that I added.
 
-var space = " ";
-var weeks = [1,2];
-var hoursworked = prompt("How many hours do you work in a week?");
-console.log("You've worked" + space + hoursworked + space + "in a week!");
-var numofweeks = prompt("How many weeks in between pay checks? \nEnter '1' for one week. \nEnter '2' for two weeks");
-console.log("Weeks between your pay checks:" + space + numofweeks);
-var A = numofweeks * weeks[0];
-var B = numofweeks * weeks[1];
-var hoursA = A * hoursworked;
-var hoursB = B * hoursworked;
+var space = " ";// This variable helps me make spaces in my strings easier.
+var weeks = [1,2];// This variable is an array holds the number of weeks in between paychecks.
+var hoursworked = prompt("How many hours do you work in a week?");// This variable is a prompt that is asking for the number of hours the user has worked in a week.
+console.log("You've worked" + space + hoursworked + space + "in a week!");// This outputs the user's input to the console.
+var numofweeks = prompt("How many weeks in between pay checks? \nEnter '1' for one week. \nEnter '2' for two weeks");// This variable is a prompt that is asking for the number of weeks between paychecks.
+console.log("Weeks between your pay checks:" + space + numofweeks);// This outputs the user's input to the console.
+var A = numofweeks * weeks[0];// This variable multiplies the user's input by the first index in the array.
+var B = numofweeks * weeks[1];// This variable multiplies the user's input by the second index in the array.
+var hoursA = A * hoursworked;// This variable multiplies variable "A" by the variable "hoursworked.
+var hoursB = B * hoursworked;// This variable multiplies variable "B" by the variable "hoursworked.
 
 if(numofweeks <= weeks[1]){
     console.log("Hours you worked this pay period:" + space + hoursA);
