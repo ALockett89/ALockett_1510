c85b654e48a89e1389bd60fe6d156ec5b66b5fa8
ALockett89 <aklockett@fullsail.edu>
2015-10-08 01:51:48 -0500

    2nd draft
    fixed the date & added my variables

--- Lockett_Anthony_Expressions_Assignment/Expressions_Assignment/js/ExpressionsAssignment.js
+++ Lockett_Anthony_Expressions_Assignment/Expressions_Assignment/js/ExpressionsAssignment.js
@@ -2,6 +2,18 @@
  Anthony Lockett
  SDI Week 2
  Expressions Assignment
- 10/0/15
+ 10/5/15
  */
 
+alert("          Here is my Week 2 submission! \n                Expressions Assignment");
+
+var space = " "
+var weeks = [1,2]
+var hoursworked = prompt("How many hours do you work in a week?")
+console.log("You've worked" + space + hoursworked + space + "in a week!")
+var numofweeks = prompt("How many weeks in between pay checks? \nEnter '1' for one week. \nEnter '2' for two weeks")
+console.log("Weeks between your pay checks:" + space + numofweeks)
+var A = numofweeks * weeks[0]
+var B = numofweeks * weeks[1]
+var hoursA = A * hoursworked
+var hoursB = B * hoursworked
