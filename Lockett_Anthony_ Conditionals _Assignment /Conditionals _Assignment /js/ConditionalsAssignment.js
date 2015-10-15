a8614cc4d8dfd498daa17ad11f310520c1bce889
ALockett89 <aklockett@fullsail.edu>
2015-10-14 22:39:31 -0500

    2nd draft
    added a few variables

--- Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -1,9 +1,13 @@
 /*
  Anthony Lockett
- SDI Week 1
- Output Assignment
+ SDI Week 3
+ Conditional Assignment
  10/12/15
  */
 
-alert("          Here is my Week 3 submission! \n               Conditionals Assignment");// Just a friendly alert that I added.
-
+alert("          Here is my Week 3 submission! \n               Conditionals Assignment");
+var space = " "
+var weeks = [1]
+var weeksBetweenPay = true
+weeksBetweenPay = confirm("Do you get paid every week? \nPress OK for Yes \nPress CANCEL for No")
+console.log("Do you get paid every week:" + space + weeksBetweenPay)
