c2e4885c3d1ca420510cda49726eb9a22bd67d67
ALockett89 <aklockett@fullsail.edu>
2015-10-14 22:35:28 -0500

    1st draft
    Setting up for my code

---
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -0,0 +1,9 @@
+/*
+ Anthony Lockett
+ SDI Week 1
+ Output Assignment
+ 10/12/15
+ */
+
+alert("          Here is my Week 3 submission! \n               Conditionals Assignment");// Just a friendly alert that I added.
+
