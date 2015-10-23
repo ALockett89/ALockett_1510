7721d316255dda90e9ab5c2e4757e6a864c3d999
ALockett89 <aklockett@fullsail.edu>
2015-10-22 22:49:55 -0500

    2nd draft
    variables added

--- Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/FunctionsAssignment.js
+++ Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/FunctionsAssignment.js
@@ -5,4 +5,8 @@
  10/19/15
  */
 
+alert("          Here is my Week 4 submission! \n               Functions Assignment");// Just a friendly alert that I added.
 
+var age = 21;// The legal age to gamble.
+var space = " ";// A variable that helps me make spaces.
+function ageplease(){// This function asks for the users age.
